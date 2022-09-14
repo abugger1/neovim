2e4d131c01de6d078feba25757858326d023b45b
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-14 17:28:04 -0400

    commit 00.00.12

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
-  require "sshep.options"
-  require "sshep.keymaps"
-  require "sshep.plugins"
-  require "sshep.colorscheme"
-  require "sshep.cmp"
-  require "sshep.lsp"
-  require("sshep.telescope")
-  require("sshep.treesitter")
-  require("sshep.autopairs")
-  require("sshep.comment")
-  require("sshep.gitsigns")
-  require("sshep.nvim-tree")
-  require("sshep.bufferline")
-
-
+require "sshep.options"
+require "sshep.keymaps"
+require "sshep.plugins"
+require "sshep.colorscheme"
+require "sshep.cmp"
+require "sshep.lsp"
+require("sshep.telescope")
+require("sshep.treesitter")
+require("sshep.autopairs")
+require("sshep.comment")
+require("sshep.gitsigns")
+require("sshep.nvim-tree")
+require("sshep.bufferline")
