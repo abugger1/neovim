71150bed6a1c23be13dd90503f252c302f40ddc9
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-14 22:01:17 -0400

    commit 00.00.12

--- lua/sshep/plugins.lua
+++ lua/sshep/plugins.lua
@@ -1,51 +1,51 @@
 local fn = vim.fn
 
-  -- Automatically install packer
-  local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-      "git",
-      "clone",
-      "--depth",
-      "1",
-      "https://github.com/wbthomason/packer.nvim",
-      install_path,
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
   }
-    print "installing packer close and reopen Neovim..."
-    vim.cmd [[packadd packer.nvim]]
-  end
+  print "installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
 
-  -- Autocommand that reloads neovim whenever you save the plugins.lua file
-  vim.cmd [[
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
     augroup packer_user_config
       autocmd!
       autocmd BufWritePost plugins.lua source <afile> | PackerSync
     augroup end
   ]]
 
-  -- Use a procted call so we don't error out on first use
-  local status_ok, packer = pcall(require, "packer")
-  if not status_ok then
-    return
-  end
+-- Use a procted call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
 
-  -- Have packer use a popup window
-  packer.init {
-    display = {
-      open_fn = function()
-        return require("packer.util").float {border = "rounded" }
-      end,
-    },
-  }
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
 
-  -- Install your plugins here
-  return packer.startup(function(use)
-    -- My plugins here
-    use "wbthomason/packer.nvim"
-    use "nvim-lua/popup.nvim"
-    use "nvim-lua/plenary.nvim"
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim"
+  use "nvim-lua/popup.nvim"
+  use "nvim-lua/plenary.nvim"
 
---  cmp plugins
+  --  cmp plugins
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-buffer"
@@ -98,7 +98,10 @@ local fn = vim.fn
 
 
   -- lualine
-  use "nvim-lualine/lualine.nvim"
+  use {
+    "nvim-lualine/lualine.nvim",
+    require = { "kyazdani42/nvim-web-devicons", opt = true }
+  }
 
 
   -- toggleterm
@@ -111,9 +114,9 @@ local fn = vim.fn
   use "LunarVim/darkplus.nvim"
 
 
-    -- Automatically setup your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-      require("packer").sync()
-    end
-  end)
+  -- Automatically setup your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
